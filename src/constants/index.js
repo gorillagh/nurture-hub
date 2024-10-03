e7223387d5e1f333ff0cb179cfe052f77d5e6cbe
Governor Narh <ron468896@gmail.com>
2024-10-03 13:35:20 +0000

    ramdom updates

---
+++ src/constants/index.js
@@ -0,0 +1,49 @@
+const K = {
+  OFFERINGS: [
+    {
+      icon: FaMusic, // Just refer to the component itself, not JSX
+      iconColor: "#5CB100",
+      title: "Music Class",
+      description:
+        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    },
+    {
+      icon: FaCalculator,
+      iconColor: "#F7BD9D",
+      title: "Math Class",
+      description:
+        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    },
+    {
+      icon: FaBookOpen,
+      iconColor: "#4FB0DC",
+      title: "English Class",
+      description:
+        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    },
+    {
+      icon: FaBook,
+      iconColor: "#6B53AB",
+      title: "Reading for Kids",
+      description:
+        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    },
+    {
+      icon: MdOutlineChangeHistory,
+      iconColor: "#45C7A7",
+      title: "History Class",
+      description:
+        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    },
+    {
+      icon: FaBookmark,
+      iconColor: "#FD9800 ",
+      title: "Active Class",
+      description:
+        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    },
+  ],
+};
+
+
+export default K
--- src/pages/home/sections/Offerings.jsx
+++ src/pages/home/sections/Offerings.jsx
@@ -3,50 +3,7 @@ import './offerings.css';
 import { FaMusic, FaCalculator, FaBookOpen, FaBook, FaBookmark } from "react-icons/fa";
 import { MdOutlineChangeHistory } from "react-icons/md";
 
-const offerings = [
-  {
-    icon: FaMusic, // Just refer to the component itself, not JSX
-    iconColor: "#5CB100",
-    title: "Music Class",
-    description:
-      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    icon: FaCalculator,
-    iconColor: "#F7BD9D",
-    title: "Math Class",
-    description:
-      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    icon: FaBookOpen,
-    iconColor: "#4FB0DC",
-    title: "English Class",
-    description:
-      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    icon: FaBook,
-    iconColor: "#6B53AB",
-    title: "Reading for Kids",
-    description:
-      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    icon: MdOutlineChangeHistory,
-    iconColor: "#45C7A7",
-    title: "History Class",
-    description:
-      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    icon: FaBookmark,
-    iconColor: "#FD9800 ",
-    title: "Active Class",
-    description:
-      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-];
+import K from "../../../constants";
 
 const Offerings = () => {
   return (
@@ -63,7 +20,7 @@ const Offerings = () => {
           </p>
         </div>
         <div className="offerings-container">
-          {offerings.map((offering, index) => {
+          {K.OFFERINGS.map((offering, index) => {
             const IconComponent = offering.icon; // Dynamically set the icon component
             return (
               <div className="offering-card" key={index}>
